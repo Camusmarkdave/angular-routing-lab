dbe31c47303442cc61200ef55f3aa6d114913443
Camusmarkdave <camusmarkdave@gmail.com>
2025-12-02 09:09:12 +0800

    added: netlify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Home } from './home/home';
 import { DataBinding } from './data-binding/data-binding';
 import { Directives } from './directives/directives';
 import { PageNotFound } from './page-not-found/page-not-found';
+import { Tailwind } from './tailwind/tailwind';
 
 
 
@@ -12,6 +13,7 @@ export const routes: Routes = [
   {path:  '', component : Home},
   {path: 'data-binding', component : DataBinding},
   {path: 'directives', component : Directives},
+  {path: 'tailwind', component : Tailwind},
   {path: '**', component : PageNotFound},
 ];
 
